19508e7bdf67cf5a53a59d811a5fc53ea2c94b51
Kelsey Bowen <kelseybowen22@gmail.com>
2026-01-20 10:37:41 -0800

    proxy creation to avoid cors issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Directs any request starting with /api to the Python server
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })

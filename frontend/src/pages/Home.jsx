19f9e3a8ae51b32144e23cd7b7a5a10133fb226f
Kelsey Bowen <kelseybowen22@gmail.com>
2026-01-26 13:27:42 -0800

    login form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar'
+import Login from './Login'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <h1>Home</h1>
+      <Login/>
     </div>
   )
 }

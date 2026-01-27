a27f0626d806e3b2bbba9592ec7de294a94779f8
Kelsey Bowen <kelseybowen22@gmail.com>
2026-01-26 17:34:43 -0800

    Routing, Navbar, Footer

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,28 +1,9 @@
 import PageHeader from "../components/PageHeader"
 
 const Login = () => {
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm();
-
-  const onSubmit = async (data) => {
-    try {
-      // Send data to Python backend
-      const response = await api.post('/auth/login', data);
-      console.log('Login Success:', response.data);
-      // TODO: Save the token and redirect to Dashboard
-    } catch (error) {
-      console.error('Login Failed:', error.response?.data?.message || 'Server error');
-    }
-  };
-
   return (
     <PageHeader title="Login" />
 
   )
 }
-
-export default Login;
+export default Login
